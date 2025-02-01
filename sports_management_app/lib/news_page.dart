41a12c39e4d647d284b7ee8344ddd7599e320dad
ishaan-007 <ishaansc2017@gmail.com>
2025-02-01 15:16:05 +0530

    final thing

--- sports_management_app/lib/news_page.dart
+++ sports_management_app/lib/news_page.dart
@@ -1,54 +1,21 @@
 import 'package:flutter/material.dart';
-//import 'package:carousel_slider/carousel_slider.dart';
-
-// class SportSphereNewsApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'SportSphere News',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//         //accentColor: Colors.green,
-//       ),
-//       home: NewsPage(),
-//     );
-//   }
-// }
 
 class NewsPage extends StatelessWidget {
   NewsPage({super.key});
 
-  final List<Map<String, String>> breakingNews = [
-    {
-      'title': 'Star Player Transfers!',
-      'image': 'https://via.placeholder.com/400x200.png?text=Breaking+News+1'
-    },
+  final List<Map<String, String>> sportsNews = [
+    {'title': 'Star Player Transfers!', 'image': 'assets/images/top5.jpg'},
     {
       'title': 'Major League Finals Tonight!',
-      'image': 'https://via.placeholder.com/400x200.png?text=Breaking+News+2'
+      'image': 'assets/images/top2.jpg'
     },
     {
       'title': 'Exclusive Training Tips from Pros!',
-      'image': 'https://via.placeholder.com/400x200.png?text=Breaking+News+3'
+      'image': 'assets/images/top3.jpg'
     },
-  ];
-
-  final List<Map<String, String>> topStories = [
     {
       'title': 'Upcoming Matches You Can’t Miss',
-      'image': 'https://via.placeholder.com/200x150.png?text=Top+Story+1'
-    },
-    {
-      'title': 'Best Sports Gadgets in 2025',
-      'image': 'https://via.placeholder.com/200x150.png?text=Top+Story+2'
-    },
-    {
-      'title': 'How to Train Like a Champion',
-      'image': 'https://via.placeholder.com/200x150.png?text=Top+Story+3'
-    },
-    {
-      'title': 'Top 10 Athletes of the Year',
-      'image': 'https://via.placeholder.com/200x150.png?text=Top+Story+4'
+      'image': 'assets/images/top4.jpg'
     },
   ];
 
@@ -56,17 +23,14 @@ class NewsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('SportSphere News'),
-            Text(
-              'SportSphere',
-              style:
-                  TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
-            ),
-          ],
+        backgroundColor: Colors.green,
+        title: Text(
+          'SportSphere News',
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'Lato',
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
       body: SingleChildScrollView(
@@ -76,114 +40,52 @@ class NewsPage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                'Breaking News',
+                'Latest Sports News',
                 style: TextStyle(
                     fontSize: 22,
                     fontWeight: FontWeight.bold,
-                    color: Colors.blue),
+                    color: Colors.black,
+                    fontFamily: 'Lato'),
               ),
             ),
-            // CarouselSlider(
-            //   options: CarouselOptions(autoPlay: true, height: 200),
-            //   items: breakingNews.map((news) {
-            //     return Card(
-            //       elevation: 4,
-            //       shape: RoundedRectangleBorder(
-            //           borderRadius: BorderRadius.circular(10)),
-            //       child: Stack(
-            //         children: [
-            //           ClipRRect(
-            //             borderRadius: BorderRadius.circular(10),
-            //             child: Image.network(
-            //               news['image']!,
-            //               fit: BoxFit.cover,
-            //               width: double.infinity,
-            //             ),
-            //           ),
-            //           Positioned(
-            //             bottom: 10,
-            //             left: 10,
-            //             child: Text(
-            //               news['title']!,
-            //               style: TextStyle(
-            //                 fontSize: 16,
-            //                 fontWeight: FontWeight.bold,
-            //                 color: Colors.white,
-            //                 backgroundColor: Colors.black54,
-            //               ),
-            //             ),
-            //           ),
-            //         ],
-            //       ),
-            //     );
-            //   }).toList(),
-            // ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Top Stories',
-                style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue),
-              ),
-            ),
-            GridView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                childAspectRatio: 3 / 2,
-              ),
-              itemCount: topStories.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10),
+            Column(
+              children: sportsNews.map((news) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0, vertical: 4.0),
+                  child: Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius:
+                              BorderRadius.vertical(top: Radius.circular(10)),
+                          child: Image.asset(
+                            news['image']!,
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 200,
+                          ),
                         ),
-                        child: Image.network(
-                          topStories[index]['image']!,
-                          fit: BoxFit.cover,
-                          width: double.infinity,
-                          height: 100,
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            news['title']!,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          topStories[index]['title']!,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 );
-              },
+              }).toList(),
             ),
           ],
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.play_circle_fill), label: 'Tutorials'),
-          BottomNavigationBarItem(icon: Icon(Icons.store), label: 'Store'),
-          BottomNavigationBarItem(icon: Icon(Icons.event), label: 'Events'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
       ),
     );
   }
